33274646844d27aac2c8f0e35cf2ac21a363741f
Manisha Sharma <mishasharma9717@gmail.com>
2024-07-18 15:40:12 +0530

    jar commit

---
+++ src/main/java/com/example/k8sproject/K8sprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8sproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sprojectApplication.class, args);
+	}
+
+}
